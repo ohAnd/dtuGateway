a57629a0218ec1641610e3aaa2292b8fe520e449
ohAnd <15704728+ohAnd@users.noreply.github.com>
2024-01-27 16:25:16 +0100

    adding secrets.h

---
+++ include/secrets.h
@@ -0,0 +1,14 @@
+#ifndef SECRETS
+#define SECRETS
+
+const char *dtu_ssid_name = "DTUBI-12345678";
+const char *dtu_ssid_password = "12345678";
+
+const char *local_ssid_name = "mySSID";
+const char *local_ssid_password = "myWifiPass";
+
+const char *local_openhab_ip = "192.168.1.1";
+
+const char *local_updateInfoWebPath = "https://raw.githubusercontent.com/ohAnd/dtuGateway/main/include/version.json?token=GHSAT0AAAAAACJFYF7FW4CYXU4R6R6FH2TEZNVEA7Q";
+
+#endif
